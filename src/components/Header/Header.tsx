8171314ad007f592e938f038290595c7339f3779
Vovchanchyk <oberigfast@gmail.com>
2022-02-07 15:07:47 +0200

    fix():eslint

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Header.module.scss';
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <button type='button' className={styles.button}>
+        connect your wallet
+      </button>
+    </header>
+  );
+}
+
+export default Header;
