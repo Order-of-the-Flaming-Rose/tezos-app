2e7394b2e675af8a38c56be17b568247243c9ad6
Vovchanchyk <oberigfast@gmail.com>
2022-02-10 19:19:35 +0200

    feat(src): add form, authReducer, authContext

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-nested-ternary */
 /* eslint-disable no-console */
-import React, { useState } from 'react';
+import React from 'react';
+import { useAuthContext } from '../../contexts/AuthContext/AuthContext';
 import FormCard from '../FormCard';
 import GetWalletCard from '../GetWalletCard';
 import SendCard from '../SendCard';
@@ -8,21 +9,12 @@ import SendCard from '../SendCard';
 import styles from './Auth.module.scss';
 
 function Auth() {
-  // eslint-disable-next-line no-unused-vars
-  const [card, setCard] = useState(2);
-  // const userdata = [
-  //     'discord: Name#0000',
-  //     'telegram: @vovchanchyk',
-  //     'wallet address: tz1RB9RXTv6vpuH9WnyyG7ByUzwiHDHGqHzq',
-  //   ];
-  const progress = card === 1 ? '0%' : card === 2 ? '50%' : '100%';
+  const {
+    state: { step },
+  } = useAuthContext();
+  console.log(step);
 
-  // const handleCard = (val: number) => {
-  //   console.log(val);
-  //   if (!val) return;
-  //   if (val > 3) return;
-  //   setCard(val);
-  // };
+  const progress = step === 1 ? '0%' : step === 2 ? '50%' : '100%';
 
   const titles = ['your discrord/telegram', 'wallet', 'summary'];
 
@@ -30,15 +22,15 @@ function Auth() {
     <div className={styles.auth}>
       <div className={styles.auth__container}>
         <div className={styles.auth__header}>
-          {titles[card - 1]}
+          {titles[step - 1]}
           <div className={styles.auth__progress} style={{ width: progress }}>
             <span className={styles.auth__progresspoint} />
           </div>
         </div>
         <div className={styles.auth__body}>
-          {card === 1 ? (
+          {step === 1 ? (
             <FormCard />
-          ) : card === 2 ? (
+          ) : step === 2 ? (
             <GetWalletCard />
           ) : (
             <SendCard />
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,46 @@
+/* eslint-disable no-console */
+/* eslint-disable react/jsx-props-no-spreading */
+import { Field } from 'formik';
+import React from 'react';
+import styles from './Input.module.scss';
+
+type TFieldTypes = {
+  field: {
+    name: string;
+    value: string;
+    onChange: () => void;
+    onBlur: () => void;
+  };
+  meta: {
+    touched: boolean;
+    error: boolean;
+  };
+};
+
+type TProps = {
+  name: string;
+};
+function Input({ name }: TProps) {
+  return (
+    <Field name={name}>
+      {({ meta, field }: TFieldTypes) => {
+        const inputClass =
+          meta.error && meta.touched
+            ? `${styles.input__field} ${styles.input__error}`
+            : `${styles.input__field}`;
+
+        return (
+          <div className={styles.input__container}>
+            {name}
+            <input type='text' {...field} className={inputClass} />
+            {meta.touched && meta.error && (
+              <span className={styles.input__error}>{meta.error}</span>
+            )}
+          </div>
+        );
+      }}
+    </Field>
+  );
+}
+
+export default Input;
