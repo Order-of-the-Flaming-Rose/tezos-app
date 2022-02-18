c52598a5a561c0f6c9bd44826769119651066c9e
Vovchanchyk <60846596+vovchanchyk@users.noreply.github.com>
2022-02-18 15:59:51 +0200

    ref(pages): home page form (#8)

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,20 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable prefer-const */
+/* eslint-disable no-console */
 /* eslint-disable no-nested-ternary */
 /* eslint-disable no-debugger */
 import React from 'react';
-import Invite from '../../components/Invite';
-import { useWalletContext } from '../../contexts/WalletContext/WalletContext';
+import { Switch, Route, Redirect, useRouteMatch } from 'react-router-dom';
+import Login from '../../components/Login';
+import SignUp from '../../components/SignUp';
+// import { useWalletContext } from '../../contexts/WalletContext/WalletContext';
 import styles from './Home.module.scss';
 
 function Home() {
-  const { auth, walletAddress } = useWalletContext();
-
+  // const { auth, walletAddress } = useWalletContext();
+  let { path, url } = useRouteMatch();
+  console.log(url);
+  const auth = true;
   return (
     <div className={styles.home}>
       <div className={styles.home__left}>
@@ -15,12 +22,16 @@ function Home() {
         <div className={styles.home__logo}>ꜩ</div>
       </div>
       <div className={styles.home__right}>
-        {walletAddress ? (
-          <Invite />
-        ) : auth ? (
-          'get wallet'
+        {!auth ? (
+          'Me'
         ) : (
-          'auth before you can get the wallet'
+          <Switch>
+            <Route exact path={`${url}/`}>
+              <Redirect to={`${url}/login`} />
+            </Route>
+            <Route path={`${url}/login`} component={Login} />
+            <Route path={`${url}/sign-up`} component={SignUp} />
+          </Switch>
         )}
       </div>
     </div>
