a360ac0d9c107dc21ccb5c1b313950124c4785b0
Vovchanchyk <oberigfast@gmail.com>
2022-03-12 11:13:40 +0200

    ref(invite,singleForm):basic input refactor

--- src/components/SingleForm/SingleForm.tsx
+++ src/components/SingleForm/SingleForm.tsx
@@ -1,17 +1,21 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 import React from 'react';
-import { Form, Formik } from 'formik';
-import Input from '../Input';
+import { Field, Form, Formik } from 'formik';
+
 import { TInput } from '../Invite/Invite';
 import styles from './SingleForm.module.scss';
 
 type TProps = {
   init: TInput;
   rules: any;
+  name: string;
 };
 
-function SingleForm({ init, rules }: TProps) {
+function SingleForm({ init, rules, name }: TProps) {
   const formHandler = (val: TInput) => console.log(val);
+  console.log(init);
 
   return (
     <Formik
@@ -20,8 +24,20 @@ function SingleForm({ init, rules }: TProps) {
       onSubmit={formHandler}
     >
       <Form className={styles.form}>
-        <Input name='telegram' />
-        <input type='submit' className={styles.form__submit} />
+        <Field name={name}>
+          {({ meta, field }: { meta: any; field: any }) => {
+            console.log(meta.error);
+            return (
+              <>
+                <input type='text' {...field} className={styles.form__input} />
+                <input type='submit' className={styles.form__submit} />
+                {meta.touched && meta.error && (
+                  <span className={styles.form__error}>{meta.error}</span>
+                )}
+              </>
+            );
+          }}
+        </Field>
       </Form>
     </Formik>
   );
