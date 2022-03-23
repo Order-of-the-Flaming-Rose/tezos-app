29d3aa2d8433e3cb557bc9726eca3e70f184a9fd
Vovchanchyk <oberigfast@gmail.com>
2022-03-23 18:42:59 +0200

    ref(header): fixed, click outside, scroll opacity

--- src/components/Activity/Activity.tsx
+++ src/components/Activity/Activity.tsx
@@ -1,29 +1,21 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable no-console */
-/* eslint-disable func-names */
 import React, { useEffect } from 'react';
 import { useOperationsContext } from '../../contexts/OperationsContext/OperationsContext';
 import styles from './Activity.module.scss';
 
 function Activity() {
-  const {scrollHandler, dataHandler, activity, loading } =
+  const { scrollHandler, dataHandler, activity, loading } =
     useOperationsContext();
 
-
   useEffect(() => {
-
     dataHandler();
   }, []);
 
   useEffect(() => {
-
     document.addEventListener('scroll', scrollHandler);
-    return function () {
+    return function scroll() {
       document.removeEventListener('scroll', scrollHandler);
     };
   }, []);
-
-
 
   return (
     <div className={styles.container}>
@@ -32,21 +24,36 @@ function Activity() {
         {activity.map((op: any) => {
           return (
             <li key={op.id} className={styles.item}>
-              {(op.type === 'transaction')?<span className={styles.item__amount}> amount : {op.amount}</span>:null}
+              {op.type === 'transaction' ? (
+                <span className={styles.item__amount}>
+                  {' '}
+                  amount : {op.amount}
+                </span>
+              ) : null}
               <span className={styles.item__time}>{op.timestamp}</span>
 
-            {(op.type === 'transaction')
-            ? <>
-            <span className={styles.item__from}>from {op.sender.address}</span>
-             <span className={styles.item__to}>to {op.target.address}</span>
-             </>
-              : null
-               } 
-              <a className={styles.item__hash} href={`https://hangzhou2net.tzkt.io/${op.hash}`} target='_blank' rel="noreferrer">view in the block explorer {op.hash}</a>
+              {op.type === 'transaction' ? (
+                <>
+                  <span className={styles.item__from}>
+                    from {op.sender.address}
+                  </span>
+                  <span className={styles.item__to}>
+                    to {op.target.address}
+                  </span>
+                </>
+              ) : null}
+              <a
+                className={styles.item__hash}
+                href={`https://hangzhou2net.tzkt.io/${op.hash}`}
+                target='_blank'
+                rel='noreferrer'
+              >
+                view in the block explorer {op.hash}
+              </a>
             </li>
           );
         })}
-        {loading? <span>loading ...</span>:null}
+        {loading ? <span>loading ...</span> : null}
       </ul>
     </div>
   );
